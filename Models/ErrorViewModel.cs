272aec6d44a52c8ea11b17b9a35f8f4668e72262
Melkam Simegn <Abelm1619@gmail.com>
2020-10-11 23:28:18 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appcodegitub_cicd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
